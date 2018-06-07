9ace093c1ab20ef7aa5de01f5a2b4871a5dec372
Adauto Barros <barros.adauto@gmail.com>
2018-06-07 02:21:22 -0300

    Testing BFS - Nullpointer

--- pratica-01/tests/graph/GraphSearcherTest.java
+++ pratica-01/tests/graph/GraphSearcherTest.java
@@ -6,6 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GraphSearcherTest {
 
+    //grafo exemplo
+    private String file1 = "src/smallGraph1.txt";
+    private String file2 = "src/graphNotConnected.txt";
+    
+    private Graph graph1, graph2;
+
     @Test
     void dfs() {
         Edge e1 = new Edge(1,2);
@@ -46,5 +52,34 @@ class GraphSearcherTest {
 
     @Test
     void bfs() {
+        graph1 = GraphCreator.createGraph(file1);
+        graph2 = GraphCreator.createGraph(file2);
+
+        // busca de um grafo normal a partir do primeiro vertice
+        String expected1 = "1 - 0 -\n"
+                + "2 - 1 1\n"
+                + "3 - 1 1\n"
+                + "4 - 2 2";
+        String output1 = GraphSearcher.bfs(graph1, 1);
+        assertEquals(expected1,output1);
+
+        // busca a partir de um vertice desconectado
+        String expected2 = "6 - 0 -";
+        String output2 = GraphSearcher.bfs(graph2, 6);
+        assertEquals(expected2,output2);
+
+        // busca de um grafo desconectado
+        String expected3 = "1 - 0 -\n"
+                + "2 - 1 1\n"
+                + "3 - 2 2\n"
+                + "4 - 2 2\n"
+                + "5 - 3 4";
+        String output3 = GraphSearcher.bfs(graph2, 1);
+        assertEquals(expected3,output3);
+
+        // iniciando a busca a partir de um vertice inexistente
+        String expected4 = "5 - 0 -";
+        String output4 = GraphSearcher.bfs(graph1, 5);
+        assertEquals(expected4,output4);
     }
 }
